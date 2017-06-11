1a8deda4bf70ac8e3c8f3aa0635f562118f7d90f
Aravind Balaji <aravind.balaji@live.com>
2017-06-12 01:30:47 +0530

    Uploaded

---
+++ K-Locks/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Strt
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Strt))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.c = New System.Windows.Forms.Label()
+        Me.s = New System.Windows.Forms.Label()
+        Me.n = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.timer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(0, 1)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(249, 30)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Keyboard Locks"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Black
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(0, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 29)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Caps"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(83, 63)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 29)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Scroll"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Black
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(166, 63)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 29)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Num"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'c
+        '
+        Me.c.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer))
+        Me.c.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.c.ForeColor = System.Drawing.Color.White
+        Me.c.Location = New System.Drawing.Point(0, 31)
+        Me.c.Name = "c"
+        Me.c.Size = New System.Drawing.Size(83, 32)
+        Me.c.TabIndex = 5
+        Me.c.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        's
+        '
+        Me.s.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer))
+        Me.s.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.s.ForeColor = System.Drawing.Color.White
+        Me.s.Location = New System.Drawing.Point(84, 31)
+        Me.s.Name = "s"
+        Me.s.Size = New System.Drawing.Size(81, 32)
+        Me.s.TabIndex = 6
+        Me.s.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'n
+        '
+        Me.n.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer))
+        Me.n.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.n.ForeColor = System.Drawing.Color.White
+        Me.n.Location = New System.Drawing.Point(166, 31)
+        Me.n.Name = "n"
+        Me.n.Size = New System.Drawing.Size(83, 32)
+        Me.n.TabIndex = 7
+        Me.n.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(199, Byte), Integer), CType(CType(25, Byte), Integer), CType(CType(25, Byte), Integer))
+        Me.Panel1.BackgroundImage = Global.K_Locks.My.Resources.Resources.cross
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Panel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel1.Location = New System.Drawing.Point(222, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(27, 27)
+        Me.Panel1.TabIndex = 0
+        '
+        'timer
+        '
+        Me.timer.Interval = 1
+        '
+        'Strt
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(249, 93)
+        Me.Controls.Add(Me.n)
+        Me.Controls.Add(Me.s)
+        Me.Controls.Add(Me.c)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Location = New System.Drawing.Point(1115, 635)
+        Me.Name = "Strt"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents c As System.Windows.Forms.Label
+    Friend WithEvents s As System.Windows.Forms.Label
+    Friend WithEvents n As System.Windows.Forms.Label
+    Friend WithEvents timer As System.Windows.Forms.Timer
+
+End Class
---
+++ K-Locks/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Strt
+
+
+    Private Sub Panel1_MouseClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Panel1.MouseClick
+        Me.Close()
+    End Sub
+
+
+    Private Sub timer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles timer.Tick
+
+        If Control.IsKeyLocked(Keys.CapsLock) Then
+
+            c.BackColor = Color.FromArgb(0, 186, 16)
+
+        Else
+            c.BackColor = Color.FromArgb(30, 30, 30)
+        End If
+
+
+        If Control.IsKeyLocked(Keys.Scroll) Then
+
+            s.BackColor = Color.FromArgb(0, 186, 16)
+
+        Else
+            s.BackColor = Color.FromArgb(30, 30, 30)
+        End If
+
+
+        If Control.IsKeyLocked(Keys.NumLock) Then
+
+            n.BackColor = Color.FromArgb(0, 186, 16)
+
+        Else
+            n.BackColor = Color.FromArgb(30, 30, 30)
+        End If
+
+    End Sub
+
+    Private Sub Strt_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        timer.Start()
+    End Sub
+End Class
